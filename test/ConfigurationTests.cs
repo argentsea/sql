5b98a9226ea581f2ac1e536c6718b403eeec7630
John Hicks <HicksJ@agency.soundtransit.org>
2018-11-14 13:11:44 -0800

    Updated tests for configuration inheritance.

--- test/ConfigurationTests.cs
+++ test/ConfigurationTests.cs
@@ -36,9 +36,12 @@ namespace ArgentSea.Sql.Test
             globalData.RetryCount.Should().Be(15, "that is the first value set in the configurationsettings.json configuration file");
 
 			var sqlDbData = sqlDbOptions.Value;
-			sqlDbData.SqlDbConnections.Length.Should().Be(2, "two conections are defined in the configuration file.");
-            sqlDbData.SqlDbConnections[0].DataConnection.GetConnectionString().Should().Contain("MyApp", "the the application name should be inherited from a the global value");
-            sqlDbData.SqlDbConnections[1].DataConnection.GetConnectionString().Should().Contain("MyOtherApp", "the the application name should be inherited from a the global value");
+            sqlDbData.SqlDbConnections.Length.Should().Be(2, "two conections are defined in the configuration file.");
+            sqlDbData.SqlDbConnections[0].DataConnectionInternal.SetAmbientConfiguration(globalData, null, null);
+            sqlDbData.SqlDbConnections[1].DataConnectionInternal.SetAmbientConfiguration(globalData, null, null);
+
+            sqlDbData.SqlDbConnections[0].DataConnection.GetConnectionString().Should().Be("Data Source=10.10.25.2;Initial Catalog=MainDb;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyApp", "this is the value inherited from global configuration settings");
+            sqlDbData.SqlDbConnections[1].DataConnection.GetConnectionString().Should().Be("Data Source=MyOtherServer;Initial Catalog=OtherDb;Connect Timeout=20;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp;Current Language=English", "this is the value inherited from global configuratoin settings");
 
 			var sqlShardData = sqlShardOptions.Value;
 			sqlShardData.SqlShardSets.Length.Should().Be(2, "there are two shard sets defined");
@@ -64,17 +67,24 @@ namespace ArgentSea.Sql.Test
 
 			var dbService = new SqlDatabases(sqlDbOptions, globalOptions, dbLogger);
 			dbService.Count.Should().Be(2, "two connections are defined in the configuration file");
+            dbService["MainDb"].ConnectionString.Should().Be("Data Source=10.10.25.2;Initial Catalog=MainDb;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyApp", "this is the value inherited from global configuratoin settings");
+            dbService["OtherDb"].ConnectionString.Should().Be("Data Source=MyOtherServer;Initial Catalog=OtherDb;Connect Timeout=20;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp;Current Language=English", "this is the value inherited from global configuratoin settings");
+            var shardLogger = NSubstitute.Substitute.For<Microsoft.Extensions.Logging.ILogger<ArgentSea.Sql.SqlShardSets<byte>>>();
 
-			var shardLogger = NSubstitute.Substitute.For<Microsoft.Extensions.Logging.ILogger<ArgentSea.Sql.SqlShardSets<byte>>>();
-
-            //var shardService = new ShardSetsBase<byte, SqlShardConnectionOptions<byte>>(sqlShardOptions, securityOptions, resilienceOptions, new DataProviderServiceFactory(), shardLogger);
             var shardService = new ArgentSea.Sql.SqlShardSets<byte>(sqlShardOptions, globalOptions, shardLogger);
 
             shardService.Count.Should().Be(2, "two shard sets are defined in the configuration file");
-			shardService["Inherited"].Count.Should().Be(2, "the configuration file has two shard connections defined on shard set Set1");
+			shardService["Inherit"].Count.Should().Be(2, "the configuration file has two shard connections defined on shard set Set1");
 			shardService["Explicit"].Count.Should().Be(2, "the configuration file has two shard connections defined on shard set Set2");
-			shardService["Inherited"][0].Read.ConnectionString.Should().Contain("MyMirror", "the global configuration metadata specifies this failover partner");
-			shardService["Explicit"][0].Read.ConnectionString.Should().Contain("rorriMyM", "the configuration file specifies this failover partner");
-		}
-	}
+
+            shardService["Inherit"][0].Read.ConnectionString.Should().Be("Data Source=10.10.23.20;Failover Partner=MyMirror;Initial Catalog=dbName2;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp", "the configuration file builds this connection string");
+            shardService["Inherit"][0].Write.ConnectionString.Should().Be("Data Source=10.10.23.20;Failover Partner=MyMirror;Initial Catalog=dbName2;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp", "the configuration file builds this connection string");
+            shardService["Inherit"][1].Read.ConnectionString.Should().Be("Data Source=MyOtherServer;Initial Catalog=dbName4;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp;ApplicationIntent=ReadOnly", "the configuration file builds this connection string");
+            shardService["Inherit"][1].Write.ConnectionString.Should().Be("Data Source=MyOtherOtherServer;Initial Catalog=dbName3;Connect Timeout=5;Type System Version=\"SQL Server 2012\";Application Name=MyOtherApp;ApplicationIntent=ReadWrite", "the configuration file builds this connection string");
+            shardService["Explicit"][0].Read.ConnectionString.Should().Be("Data Source=localhost;Failover Partner=;Initial Catalog=MyDb1;Integrated Security=False;Persist Security Info=False;User ID=;Password=;Pooling=True;Min Pool Size=0;Max Pool Size=100;MultipleActiveResultSets=False;Replication=False;Connect Timeout=2;Encrypt=False;TrustServerCertificate=True;Load Balance Timeout=0;Packet Size=8000;Type System Version=Latest;Application Name=MyWebApp2;Current Language=english;Workstation ID=;ApplicationIntent=ReadWrite;MultiSubnetFailover=True", "the configuration file builds this connection string");
+            shardService["Explicit"][0].Write.ConnectionString.Should().Be(shardService["Explicit"][0].Read.ConnectionString, "the read and write connections should be identical");
+            shardService["Explicit"][1].Read.ConnectionString.Should().Be("Data Source=localhost;Failover Partner=rorriMyM;Initial Catalog=MyDb2;Integrated Security=False;Persist Security Info=False;User ID=;Password=;Pooling=True;Min Pool Size=0;Max Pool Size=100;MultipleActiveResultSets=False;Replication=False;Connect Timeout=2;Encrypt=False;TrustServerCertificate=True;Load Balance Timeout=0;Packet Size=8000;Type System Version=Latest;Application Name=MyWebApp3;Current Language=english;Workstation ID=;ApplicationIntent=ReadOnly;MultiSubnetFailover=True", "the configuration file builds this connection string");
+            shardService["Explicit"][1].Write.ConnectionString.Should().Be("Data Source=localhost;Failover Partner=;Initial Catalog=MyDb3;Integrated Security=False;Persist Security Info=False;User ID=;Password=;Pooling=True;Min Pool Size=0;Max Pool Size=100;MultipleActiveResultSets=False;Replication=False;Connect Timeout=2;Encrypt=False;TrustServerCertificate=True;Load Balance Timeout=0;Packet Size=8000;Type System Version=Latest;Application Name=MyWebApp4;Current Language=english;Workstation ID=;ApplicationIntent=ReadWrite;MultiSubnetFailover=True", "the configuration file builds this connection string");
+        }
+    }
 }
